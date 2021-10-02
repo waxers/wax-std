fc9f713582320ceaeda44e12981d97d05dfd93ac
Defmc <defmc0101@gmail.com>
2021-10-02 13:34:39 -0400

    style(core)!: mover aliases de tipos numéricos

---
+++ src/core/sys/intdefs.hpp
@@ -0,0 +1,21 @@
+#include <cstdint>
+
+namespace wax {
+    typedef uint8_t u8;
+    typedef int8_t i8;
+
+    typedef uint16_t u16;
+    typedef int16_t i16;
+
+    typedef uint32_t u32;
+    typedef int32_t i32;
+  
+    typedef uint64_t u64;
+    typedef int64_t i64;
+
+    typedef unsigned int usize;
+    typedef int isize;
+
+    typedef intmax_t imax;
+    typedef uintmax_t umax;
+}
