94f88e0c3432e304f3db6987b92ba5bda37ce829
Defmc <defmc0101@gmail.com>
2021-10-02 11:04:47 -0400

    feat(core): adicionar wrapper temporário para controle de memória dinâmica

---
+++ src/night/alloc.hpp
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "intdefs.hpp"
+
+namespace wax {
+    namespace mem {
+        template <class T>
+        T* alloc(usize count = 1);
+
+        template <class T>
+        T* realloc(T& ptr, usize new_count);
+
+        template <class T>
+        void free(T& ptr);
+    }
+}
