c84648e10d81709dc53b69612f30c97a5fb0420a
Defmc <defmc0101@gmail.com>
2021-10-02 13:35:23 -0400

    style(test)!: mover sistema de unit-testing

---
+++ src/test/test.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+
+#if defined(USE_COLORED_ASSERTIONS)
+#define WHITE "\u001b[37m\033[1m"
+#define RED "\u001b[31m\033[1m"
+#define GREEN "\u001b[32m\033[1m"
+#define RESET "\u001b[0m\033[0m"
+#else
+#define RED ""
+#define WHITE ""
+#define GREEN ""
+#define RESET ""
+#endif
+
+namespace wax {
+    namespace test {
+        void finish_tests();
+    }
+}
+
+#include "test.cpp"
