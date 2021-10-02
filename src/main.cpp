28480926783792dd340b61aad67a28370b3d8f40
Defmc <defmc0101@gmail.com>
2021-10-02 13:39:15 -0400

    style(iden): retirar tabs excecivas

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 
 #define USE_COLORED_ASSERTIONS
-#include "night/test.hpp"
+#include <night/test.hpp>
+#include <night/alloc.hpp>
+#include <night/intdefs.hpp>
 
+wax::i32 main(){
+    wax::u8* x = wax::mem::alloc<wax::u8>();
+    assert(0b1 == 1);
+    assert(9 == 3 * 3);
+    assert(true == true);
+    assert(true != !false);
 
-int main(){
-        assert(0b1 == 1);
-        assert(9 == 3 * 3);
-        assert(true == true);
-        assert(true != !false);
+    std::cout << std::endl;
 
-        std::cout << std::endl;
-
-        wax::test::finish_tests();
-	return 0;
+    wax::test::finish_tests();
+    return 0;
 }
