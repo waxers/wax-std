81a22eaf788df8e025a4b4fac493e1d76b1e1d6a
GustavoGLD <gustavo.relativistico@gmail.com>
2021-10-02 11:29:35 -0300

    feat(String): cadeias de caracter com POO e polimorfismo

---
+++ src/night/string.hpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+
+#include "intdefs.hpp"
+
+namespace wax {
+    class String {
+    public:
+        String(const char* str);
+
+        const char* c_str();
+
+        usize size();
+
+        void operator=(const char* str);
+
+        void operator=(String str);
+
+        void operator+=(const char* str);
+
+        void operator+=(String str);
+
+    private:
+        const char* str;
+    };
+}
