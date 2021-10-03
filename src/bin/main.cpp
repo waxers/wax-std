358495d547f3d6e7b339cf288836666b8457ee91
Defmc <defmc0101@gmail.com>
2021-10-03 18:07:26 -0400

    fix(test): fragmentar testes em múltiplos arquivos, sendo para cada espécie de teste respectiva ao seu nome/função

--- src/bin/main.cpp
+++ src/bin/main.cpp
@@ -6,44 +6,15 @@
 #include <core/sys/intdefs.hpp>
 #include <night/string.hpp>
 
-wax::i32 main(){
-    wax::u8* x = wax::mem::alloc<wax::u8>();
+#include "tests/string.hpp"
+#include "tests/box.hpp"
 
-    //testing string's operators == and !=
-    assert(wax::String("text") == wax::String("text"));
-    assert(wax::String("text") != wax::String("...."));
-    assert(wax::String("text") == "text");
-    assert("text" == wax::String("text"));
-    assert(wax::String("text") != "....");
-    assert("text" != wax::String("..."));
+    wax::i32 main(){
+        tests::string(); // FIXME: Memory Leak em toda chamada do operador `+`
+        tests::box();
 
-    //testing string's operator +
-    assert(wax::String("text") == wax::String("te") + wax::String("xt"));
-    assert(wax::String("text") == wax::String("te") + "xt");
-    assert(wax::String("text") == "te" + wax::String("xt"));
-    assert(wax::String("te") + wax::String("xt") == wax::String("text"));
-    assert(wax::String("te") + "xt" == wax::String("text"));
-    assert("te" + wax::String("xt") == wax::String("text"));
+        std::cout << std::endl;
 
-    //testing string's initializations
-    wax::String text1 = "text"; assert(text1 == "text");
-    wax::String text2 = text1;  assert(text2 == "text");
-    wax::String text3; text3 = "text"; assert(text3 == "text");
-
-    //testing string's transformations
-    assert(strcmp(text1.c_str(), "text") == 0);
-    assert(strcmp((const char*)text1, "text") == 0);
-    wax::String char1 = "t"; assert((char)char1 == 't');
-
-    //testing string's operator []
-    wax::String abc = "abc";
-    assert(abc[0] == 'a');
-    assert(abc[1] == 'b');
-    assert(abc[2] == 'c');
-
-
-    std::cout << std::endl;
-
-    wax::test::finish_tests();
+        wax::test::finish_tests();
     return 0;
 } 
