b7a4f2cda6659a5214fc5ea22734e910ce3c75d1
GustavoGLD <gustavo.relativistico@gmail.com>
2021-10-03 16:38:31 -0300

    feat(io): print() e println()

---
+++ src/night/io.cpp
@@ -0,0 +1,40 @@
+#include "io.hpp"
+
+namespace wax {
+    void OStream::operator<<(wax::String ostream) {
+        printf(ostream.c_str());
+    }
+    void OStream::operator<<(const char* ostream) {
+        printf(ostream);
+    }
+    void OStream::operator<<(char ostream) {
+        printf("%c", ostream);
+    }
+    void OStream::operator<<(int ostream) {
+        printf("%i", ostream);
+    }
+    void OStream::operator<<(unsigned ostream) {
+        printf("%i", ostream);
+    }
+    void OStream::operator<<(short ostream) {
+        printf("%i", ostream);
+    }
+    void OStream::operator<<(unsigned short ostream) {
+        printf("%i", ostream);
+    }
+    void OStream::operator<<(long ostream) {
+        printf("%i", ostream);
+    }
+    void OStream::operator<<(unsigned long ostream) {
+        printf("%i", ostream);
+    }
+    void OStream::operator<<(float ostream) {
+        printf("%f", ostream);
+    }
+    void OStream::operator<<(double ostream) {
+        printf("%f", ostream);
+    }
+    void OStream::operator<<(bool ostream) {
+        ostream ? printf("true") : printf("false");
+    }
+}
---
+++ src/night/io.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <night/string.hpp>
+
+namespace wax {
+    struct OStream {
+        void operator<<(String ostream);
+        void operator<<(const char* ostream);
+        void operator<<(char ostream);
+        void operator<<(int ostream);
+        void operator<<(unsigned ostream);
+        void operator<<(short ostream);
+        void operator<<(unsigned short ostream);
+        void operator<<(long ostream);
+        void operator<<(unsigned long ostream);
+        void operator<<(float ostream);
+        void operator<<(double ostream);
+        void operator<<(bool ostream);
+    };
+    OStream cout;
+
+    void print() {}
+    template<typename T, typename ...Args> 
+    void print(T arg1, Args... args) {
+        wax::cout << arg1;
+        wax::print(args...);
+    }
+
+    void println() { wax::cout << "\n"; }
+    template<typename T, typename ...Args> 
+    void println(T arg1, Args... args) {
+        wax::cout << arg1;
+        wax::println(args...);
+    }
+}
