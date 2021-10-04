66241a978cbfdac2a2ee1a6a03ff54b7fa57d15f
GustavoGLD <gustavo.relativistico@gmail.com>
2021-10-04 14:45:30 -0300

    feat(Pair): uma tupla simples de 2 elementos

---
+++ src/night/pair.hpp
@@ -0,0 +1,41 @@
+#include <night/string.hpp>
+#include <night/io.hpp>
+
+namespace wax {
+    template <class T1, class T2>
+    class Pair {
+    public:
+
+        Pair(T1 first, T2 second): first(first), second(second) {}
+
+        Pair(const Pair<int, char>& pair): first(pair.first), second(pair.second) {}
+
+        void operator=(Pair pair) {
+            this->first = pair.first;
+            this->second = pair.second;
+        }
+
+        T1 first;
+        T2 second;
+    };
+
+    template <class T1, class T2>
+    Pair<T1, T2> MakePair(T1 first, T2 second) {
+        return Pair<T1, T2>(first, second);
+    }
+
+    template <class T1, class T2>
+    void operator<<(wax::OStream cout, Pair<T1, T2> pair) {
+        println("first:", pair.first, "| second:", pair.second);
+    }
+
+    template <class T1, class T2>
+    bool operator==(Pair<T1, T2> pair1, Pair<T1, T2> pair2) {
+        return ((pair1.first == pair2.first) && (pair1.second == pair2.second));
+    }
+
+    template <class T1, class T2>
+    bool operator!=(Pair<T1, T2> pair1, Pair<T1, T2> pair2) {
+        return (pair1 == pair2) == false;
+    }
+}
