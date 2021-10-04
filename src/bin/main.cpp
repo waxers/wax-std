6933eb33cf27d74be3b32c394ca301d7949ba44d
GustavoGLD <gustavo.relativistico@gmail.com>
2021-10-04 14:51:51 -0300

    feat(tests): testes para wax::Pair

--- src/bin/main.cpp
+++ src/bin/main.cpp
@@ -9,11 +9,13 @@
 #include "tests/string.hpp"
 #include "tests/box.hpp"
 #include "tests/print.hpp"
+#include "tests/pair.hpp"
 
 wax::i32 main(){
         tests::string(); // FIXME: Memory Leak em toda chamada do operador `+`
         tests::box();
         //tests::print_println();
+        //tests::pair();
 
         std::cout << std::endl;
 
