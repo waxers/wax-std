735f109f415524cf59bc2e39c87d5bdf522d552a
GustavoGLD <gustavo.relativistico@gmail.com>
2021-10-05 15:24:43 -0300

    feat(Any): trabalhando com 'tipagem dinamica'

---
+++ src/night/any.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace wax {
+    struct Any {
+        Any() {
+        }
+
+        template<class T>
+        Any(const T& variable_) {
+            delete static_cast<char*>(raw_ptr);
+            raw_ptr = new T(variable_);
+        }
+
+        template<class T>
+        void operator=(const T& variable_) {
+            delete static_cast<char*>(raw_ptr);
+            raw_ptr = new T(variable_);
+        }
+
+        template<class T>
+        T get() {
+            return *static_cast<T*>(raw_ptr);
+        }
+
+        ~Any() {
+            delete static_cast<char*>(raw_ptr);
+        }
+
+        void* raw_ptr = new char(0);
+    };
+}
